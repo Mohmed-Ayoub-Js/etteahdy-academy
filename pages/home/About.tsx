fb74bd04a6e726a453d0e906199eb8cf0f15d315
MohmedAyoubJs <113200512+Mohmed-Ayoub-Js@users.noreply.github.com>
2024-06-14 00:11:07 +0200

    Add files via upload

--- pages/home/About.tsx
+++ pages/home/About.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const About = () => {
   return (
-    <div style={{backgroundImage:'url("https://scontent.forn3-2.fna.fbcdn.net/v/t39.30808-6/279972353_518122783349048_8616702938511215802_n.jpg?_nc_cat=100&ccb=1-7&_nc_sid=5f2048&_nc_ohc=aCWZsE744HEQ7kNvgE4Ep0G&_nc_ht=scontent.forn3-2.fna&oh=00_AYA8kl9pYY1R4uFPgdBdBRGfJWmAD4ywS34XHG4k6VztHw&oe=6668D945")'}} className=' h-[100vh] w-full'>
+    <div style={{backgroundImage:'url("./one.jpg")'}} className=' h-[100vh] w-full'>
        
     </div>
   )
--- pages/home/FromPast.tsx
+++ pages/home/FromPast.tsx
@@ -2,12 +2,10 @@ import React from 'react'
 
 const FromPast = () => {
   const data = [
-    "https://scontent.forn3-2.fna.fbcdn.net/v/t39.30808-6/434078527_931471758983334_7001921732659593430_n.jpg?_nc_cat=111&ccb=1-7&_nc_sid=5f2048&_nc_ohc=PrEZ0FQpDLoQ7kNvgFNtgLl&_nc_ht=scontent.forn3-2.fna&oh=00_AYB3QQt8qKVIsrmQEvOkEN6svbRr4BR8zQu6aBzpP4-nwQ&oe=6668E319",
-    "https://scontent.forn3-5.fna.fbcdn.net/v/t39.30808-6/402556261_836471161816728_6270921720596547529_n.jpg?_nc_cat=105&ccb=1-7&_nc_sid=5f2048&_nc_ohc=QkImsxzI2dgQ7kNvgHI0lsU&_nc_ht=scontent.forn3-5.fna&oh=00_AYCvj1sbCc_G7DyhCpncC6iLwfRTC7U5ofSpOSlDLBKeGQ&oe=66690CBA",
-    "https://scontent.forn3-2.fna.fbcdn.net/v/t39.30808-6/358703426_750239597106552_8918322343800627526_n.jpg?_nc_cat=100&ccb=1-7&_nc_sid=5f2048&_nc_ohc=wLqYIjlvoKMQ7kNvgGV2ulK&_nc_ht=scontent.forn3-2.fna&oh=00_AYDpgUM2_nMy4yhxhOm9sulCTYUUk88G-kYe3oGGYogbug&oe=66691528",
-    "https://scontent.forn3-1.fna.fbcdn.net/v/t39.30808-6/358714768_750239457106566_6281150561083763356_n.jpg?_nc_cat=106&ccb=1-7&_nc_sid=5f2048&_nc_ohc=Ks_pSrzTuocQ7kNvgEKUCfd&_nc_ht=scontent.forn3-1.fna&oh=00_AYCsGdHGJJ3_drcMSu2a9U5aL-eHpkT_R_fFGuiHXbWkaA&oe=6668D8D5",
-    "https://scontent.forn3-5.fna.fbcdn.net/v/t39.30808-6/352167777_615051267253236_7249396625996630027_n.jpg?_nc_cat=101&ccb=1-7&_nc_sid=5f2048&_nc_ohc=TWO6HZh_F1EQ7kNvgG3jx2L&_nc_ht=scontent.forn3-5.fna&oh=00_AYCLA4z7jzwtoSXAd4uBfUqO0Q3SrpymJY13eNi-ilrWOQ&oe=6668FACE",
-    "https://scontent.forn3-5.fna.fbcdn.net/v/t39.30808-6/328690929_588215562726721_2026803279811955751_n.jpg?_nc_cat=103&ccb=1-7&_nc_sid=5f2048&_nc_ohc=Ugp2uyjiHXYQ7kNvgGC4-6K&_nc_ht=scontent.forn3-5.fna&oh=00_AYDpAc6bto-Pu4WIAlWn_1r00pA8gYHlKQQ-qN0ZOS2UzA&oe=6668FEFD",
+    "./three.jpg",
+    "./five.jpg",
+    "./four.jpg",
+    "./three.jpg",
   ];
   return (
     <div id='img' className=' flex justify-center items-center flex-col p-5'>
